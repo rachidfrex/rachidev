d9da24436493b29818955b780d72e5a6eaf9a365
rachidfrex <2003043000250@ofppt-edu.ma>
2024-03-09 19:24:58 +0100

    Update header styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import graph from "../assets/graph.png";
 function Header() {
   return (
-    <div className="flex w-full text-white justify-center relative z-50  ">
+    <div className="flex w-full text-white justify-center  z-50  ">
       <h1 className=" text-9xl font-bold  p-5 float-left w-[50%] text-start flex justify-end items-end  ">
         Hello I am Rachid
 
         
       </h1>
-      <span className=" absolute  right-0 bottom-0 -z-10">
+      <span className=" absolute top-0 right-0 bottom-0 -z-10">
         <img src={graph} alt
         ="graph" className="w-[100%]"  />
         </span>
